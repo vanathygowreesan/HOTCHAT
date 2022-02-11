a68382b599bc84c75c1301b8875c6ed54b0e9b05
Vanathy <vanathy.gowreesan@dcmail.ca>
2022-02-10 20:17:27 -0500

    Changed menu id from 1 to primary

--- themes/hotcoffee/functions.php
+++ themes/hotcoffee/functions.php
@@ -49,7 +49,7 @@ function hotcoffee_setup() {
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus(
 		array(
-			'menu-1' => esc_html__( 'Primary', 'hotcoffee' ),
+			'menu-primary' => esc_html__( 'Primary', 'hotcoffee' ),
 		)
 	);
 
--- themes/hotcoffee/header.php
+++ themes/hotcoffee/header.php
@@ -48,7 +48,7 @@
 			<?php
 			wp_nav_menu(
 				array(
-					'theme_location' => 'menu-1',
+					'theme_location' => 'menu-primary',
 					'menu_id'        => 'primary-menu',
 				)
 			);

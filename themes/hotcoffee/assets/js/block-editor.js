00ccd259d38001d7cf6f016109e59aca9de7305b
Vanathy <vanathy.gowreesan@dcmail.ca>
2022-02-14 17:21:48 -0500

    block-editor.js created for the block style

--- themes/hotcoffee/assets/js/block-editor.js
+++ themes/hotcoffee/assets/js/block-editor.js
@@ -1,1 +1,11 @@
-console.log('Block Editor');
+//console.log('Block Editor');
+
+wp.blocks.registerBlockStyle( 'core/quote', {
+    name: 'fancy-quote',
+    label: 'Fancy Quote',
+} );
+
+wp.domReady( function () {
+    wp.blocks.unregisterBlockStyle( 'core/quote', 'large' );
+    wp.blocks.unregisterBlockStyle( 'core/quote', 'plain' )
+} ); 

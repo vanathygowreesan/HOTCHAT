9a261e33dabe1c16160cc6151be9bbc9f7b25007
Vanathy <vanathy.gowreesan@dcmail.ca>
2022-02-14 17:20:41 -0500

    Block editor style created for fancy quote

--- themes/hotcoffee/inc/block-editor.php
+++ themes/hotcoffee/inc/block-editor.php
@@ -14,3 +14,11 @@ function hotcoffee_enqueue_block_editor_assets() {
     );
 }
 add_action( 'enqueue_block_editor_assets', 'hotcoffee_enqueue_block_editor_assets' );
+
+function hotcoffee_enqueue_block_assets() {
+    wp_enqueue_style( 
+        'block-editor-style', 
+        get_template_directory_uri() . '/assets/css/block-editor.css',
+    );
+}
+add_action( 'enqueue_block_assets', 'hotcoffee_enqueue_block_assets' );

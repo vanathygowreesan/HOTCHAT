d6e9954701abcc61d3e2f92001e6068d9404f48e
Vanathy <vanathy.gowreesan@dcmail.ca>
2022-02-14 19:33:43 -0500

    enqueue_style, script created

--- themes/hotcoffee/functions.php
+++ themes/hotcoffee/functions.php
@@ -126,7 +126,35 @@ add_action( 'widgets_init', 'hotcoffee_widgets_init' );
  * Enqueue scripts and styles.
  */
 function hotcoffee_scripts() {
-	wp_enqueue_style( 'hotcoffee-style', get_stylesheet_uri(), array(), HOTCOFFEE_VERSION );
+	wp_enqueue_style(
+		 'hotcoffee-style', 
+		get_stylesheet_uri(),
+		array(), 
+		HOTCOFFEE_VERSION 
+	);
+
+	wp_enqueue_style( 
+		'foundation-style', 
+		get_template_directory_uri() . '/assets/css/vendor/foundation.min.css',
+		array(), 
+		'6.7.4'
+	);
+
+	wp_enqueue_script( 
+		'what-input-script', 
+		get_template_directory_uri() . '/assets/js/vendor/what-input.js',
+		array('jquery'), 
+		'5.2.10',
+		true
+	);
+
+	wp_enqueue_script( 
+		'foundation-script', 
+		get_template_directory_uri() . '/assets/js/vendor/foundation.min.js',
+		array('jquery'), 
+		'6.7.4',
+		true
+	);
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
@@ -153,5 +181,7 @@ require get_template_directory() . '/inc/customizer.php';
  * Block editor additions.
  */
 require get_template_directory() . '/inc/block-editor.php';
+
+
 
 

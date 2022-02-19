8c04e37c3af9eb6588b6a3671978e38de1763142
Vanathy <vanathy.gowreesan@dcmail.ca>
2022-02-18 22:08:36 -0500

    header content added in header.php

--- themes/hotcoffee/header.php
+++ themes/hotcoffee/header.php
@@ -44,14 +44,32 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'hotcoffee' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-primary',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
+			<div class="grid-x grid-padding-x grid-margin-x top-nav">
+				<div class="large-4 cell"></div>
+				<div class="large-4 cell logo">
+					<img src="http://hotchat.local/wp-content/themes/hotcoffee/assets/img/logo_final.png">
+				</div>
+				<div class="large-4 cell order-links">
+					<a href=""><img src="http://hotchat.local/wp-content/themes/hotcoffee/assets/img/search_icon.png"></a>&nbsp; &nbsp;
+					<a href=""><img src="http://hotchat.local/wp-content/themes/hotcoffee/assets/img/user.png"></a>&nbsp; &nbsp;
+					<a href=""><img src="http://hotchat.local/wp-content/themes/hotcoffee/assets/img/Cart.png"></a>
+				</div>
+        	</div>
+
+			<div class="grid-x grid-padding-x grid-margin-x bottom-nav">
+				<div class="large-4 cell"></div>
+				<div class="large-4 cell menu-links">
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'hotcoffee' ); ?></button>
+				<?php
+					wp_nav_menu(
+						array(
+							'theme_location' => 'menu-primary',
+							'menu_id'        => 'primary-menu',
+						)
+					);
+				?>
+				</div>
+				<div class="large-4 cell"></div>
+        	</div>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->

69f7ede03011e8dd8991c3b599e95b939fe9bda8
Vanathy <vanathy.gowreesan@dcmail.ca>
2022-03-08 12:03:15 -0500

    re-added the product title

--- themes/hotcoffee/inc/woocommerce.php
+++ themes/hotcoffee/inc/woocommerce.php
@@ -13,3 +13,15 @@ function hotcoffee_use_block_editor_for_post_type ($use_block_editor, $post_type
     return $use_block_editor;
 }
  add_filter('use_block_editor_for_post_type', 'hotcoffee_use_block_editor_for_post_type', 10, 2);
+
+ /**
+  * remove default woocommerce style
+  */
+
+  // add_filter( 'woocommerce_enqueue_styles', '__return_false' );
+
+   /**
+  * re-add the product title
+  */
+
+  add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title', 4);

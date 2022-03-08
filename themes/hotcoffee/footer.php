82f1e421775cd4cd1211cf47991f140f035cfe38
Vanathy <vanathy.gowreesan@dcmail.ca>
2022-03-07 19:46:24 -0500

    footer dynamic content fully developed

--- themes/hotcoffee/footer.php
+++ themes/hotcoffee/footer.php
@@ -13,54 +13,185 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="grid-x grid-padding-x grid-margin-x top-footer-wrapper">
-			<div class="large-1 cell footer-first-col">
+			
+			<div class="large-3 cell footer-first-col">
+				<div class="site-branding">
+					<?php
 
-			</div>
-			<div class="large-2 cell footer-first-col">
-				<img src="http://hotchat.local/wp-content/themes/hotcoffee/assets/img/logo_final.png">
+					if (! empty (get_custom_logo())){
+						the_custom_logo();
+
+					}else{
+
+						if ( is_front_page() && is_home() ) :
+							?>
+							<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+							<?php
+						else :
+							?>
+							<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+							<?php
+						endif;
+						$hotcoffee_description = get_bloginfo( 'description', 'display' );
+						if ( $hotcoffee_description || is_customize_preview() ) :
+							?>
+							<p class="site-description"><?php echo $hotcoffee_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+							<?php 
+			
+						endif; 
+					}
+							?>
+				</div><!-- .site-branding -->
+
 				<p>100, Ontario Street, Ontario L1C1B1 <br>
 				<img src="http://hotchat.local/wp-content/themes/hotcoffee/assets/img/phon_icon.png">  : 298-111-1111</p>
-				<img src="http://hotchat.local/wp-content/themes/hotcoffee/assets/img/socila icons.png">
+				<ul class="social-media-menu">
+					<?php if (!empty (get_theme_mod('hotcoffee_facebook_url')) && !empty(get_theme_mod ('hotcoffee_facebook_title'))){ ?>
+						<li>
+							<a href="<?php echo esc_url (get_theme_mod('hotcoffee_facebook_url')); ?>">
+							<?php
+							if (!empty (get_theme_mod('hotcoffee_facebook_icon'))){
+								echo wp_get_attachment_image (get_theme_mod('hotcoffee_facebook_icon'), array('40', '40'));
+							} else {
+								echo get_theme_mod('hotcoffee_facebook_title');
+							}
+							?>
+							</a>
+						</li>
+
+					<?php } ?>
+
+					<?php if (!empty (get_theme_mod('hotcoffee_twitter_url')) && !empty(get_theme_mod ('hotcoffee_twitter_title'))){ ?>
+						<li>
+							<a href="<?php echo esc_url (get_theme_mod('hotcoffee_twitter_url')); ?>">
+							<?php
+							if (!empty (get_theme_mod('hotcoffee_twitter_icon'))){
+								echo wp_get_attachment_image (get_theme_mod('hotcoffee_twitter_icon'), array('40', '40'));
+							} else {
+								echo get_theme_mod('hotcoffee_twitter_title');
+							}
+							?>
+							</a>
+						</li>
+					<?php } ?>
+
+					<?php if (!empty (get_theme_mod('hotcoffee_whatsapp_url')) && !empty(get_theme_mod ('hotcoffee_whatsapp_title'))){ ?>
+						<li>
+							<a href="<?php echo esc_url (get_theme_mod('hotcoffee_whatsapp_url')); ?>">
+							<?php
+							if (!empty (get_theme_mod('hotcoffee_whatsapp_icon'))){
+								echo wp_get_attachment_image (get_theme_mod('hotcoffee_whatsapp_icon'), array('40', '40'));
+							} else {
+								echo get_theme_mod('hotcoffee_whatsapp_title');
+							}
+							?>
+							</a>
+						</li>
+					<?php } ?>
+
+					<?php if (!empty (get_theme_mod('hotcoffee_instagram_url')) && !empty(get_theme_mod ('hotcoffee_instagram_title'))){ ?>
+						<li>
+							<a href="<?php echo esc_url (get_theme_mod('hotcoffee_instagram_url')); ?>">
+							<?php
+							if (!empty (get_theme_mod('hotcoffee_instagram_icon'))){
+								echo wp_get_attachment_image (get_theme_mod('hotcoffee_instagram_icon'), array('40', '40'));
+							} else {
+								echo get_theme_mod('hotcoffee_instagram_title');
+							}
+							?>
+							</a>
+						</li>
+					<?php } ?>
+
+					<?php if (!empty (get_theme_mod('hotcoffee_youtube_url')) && !empty(get_theme_mod ('hotcoffee_youtube_title'))){ ?>
+						<li>
+							<a href="<?php echo esc_url (get_theme_mod('hotcoffee_youtube_url')); ?>">
+							<?php
+							if (!empty (get_theme_mod('hotcoffee_youtube_icon'))){
+								echo wp_get_attachment_image (get_theme_mod('hotcoffee_youtube_icon'), array('40', '40'));
+							} else {
+								echo get_theme_mod('hotcoffee_youtube_title');
+							}
+							?>
+							</a>
+						</li>
+					<?php } ?>
+				</ul> <!-- close social media ul -->
+
+				<?php
+				/*if (has_nav_menu( 'menu-social' )){
+					wp_nav_menu(
+						array(
+							'theme_location' => 'menu-social',
+						)
+					);
+				} */
+				?>
 			</div>
+
+			<!-- footer 2nd column dynamic content -->
 			<div class="large-2 cell footer-second-col">
-				<h4 class="footer-heading">Help & Information</h4>
+				<h4 class="footer-heading">Useful Information</h4>
 				<ul class="footer-list">
-					<li><a href="">About Us</a></li>
-					<li><a href="">Privacy Policy</a></li>
-					<li><a href="">Terms & Conditions</li></a>
-					<li><a href="">Products Return</li></a>
-					<li><a href="">Wholesale Policy</li></a>
-				</ul>
-			</div>
-			
-			<div class="large-2 cell footer-third-col">
-				<h4 class="footer-heading">Help & Information</h4>
-				<ul class="footer-list">
-					<li><a href="">Return Policies</a></li>
-					<li><a href="">Terms & Conditions</a></li>
-					<li><a href="">Contact</li></a>
-					<li><a href="">Accessories</li></a>
-					<li><a href="">Store Locations</li></a>
+					<li>
+						<?php
+						if (has_nav_menu( 'menu-footer1' )){
+							wp_nav_menu(
+								array(
+									'theme_location' => 'menu-footer1',
+								)
+							);
+						}
+						?>
+					</li>
 				</ul>
 			</div>
 
+	
+			<!-- footer 3rd column dynamic content -->
+			<div class="large-2 cell footer-third-col">
+				<h4 class="footer-heading">Contact Information</h4>
+				
+				<ul class="footer-list">
+					<li>
+						<?php
+						if (has_nav_menu( 'menu-footer2' )){
+							wp_nav_menu(
+								array(
+									'theme_location' => 'menu-footer2',
+								)
+							);
+						}
+						?>
+					</li>
+				</ul>
+			</div>
+
+			<!-- footer 4th column dynamic content -->
 			<div class="large-2 cell footer-fourth-col">
 				<h4 class="footer-heading">Popular Categories</h4>
 				<ul class="footer-list">
-					<li><a href="">Arabica</a></li>
-					<li><a href="">Decaf</a></li>
-					<li><a href="">Espresso</li></a>
-					<li><a href="">Latte</li></a>
-					<li><a href="">Cappuccino</li></a>
+					<li>
+						<?php
+						if (has_nav_menu( 'menu-footer3' )){
+							wp_nav_menu(
+								array(
+									'theme_location' => 'menu-footer3',
+								)
+							);
+						}
+						?>
+					</li>
 				</ul>
 			</div>
-			<div class="large-2 cell footer-fourth-col">
+
+			<div class="large-3 cell footer-fifth-col">
 				<img src="http://hotchat.local/wp-content/themes/hotcoffee/assets/img/coffee-cup.png">
 			</div>
         </div>
 
 		<div class="grid-x grid-padding-x grid-margin-x">
-			<div class="large-12 cell copy-rights">
+			<div class="large-12 small-12 cell copy-rights">
 				<p>Copyright © Hot Chat . All rights reserved.</p>
 			</div>
         </div>

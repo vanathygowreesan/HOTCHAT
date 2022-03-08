0b1cc2861f4756882ecac4ffd86b561c7c7bdb42
Vanathy <vanathy.gowreesan@dcmail.ca>
2022-03-07 19:47:18 -0500

    woocommerce converted to block-editor

--- themes/hotcoffee/inc/woocommerce.php
+++ themes/hotcoffee/inc/woocommerce.php
@@ -4,3 +4,12 @@
  *
  * @package Fresh_Coffee
  */
+
+
+function hotcoffee_use_block_editor_for_post_type ($use_block_editor, $post_type){
+    if('product'===$post_type){
+        $use_block_editor = true;
+    }
+    return $use_block_editor;
+}
+ add_filter('use_block_editor_for_post_type', 'hotcoffee_use_block_editor_for_post_type', 10, 2);

9c09c62527c42845d2391f3952b87acec472a047
Vanathy <vanathy.gowreesan@dcmail.ca>
2022-03-07 19:46:07 -0500

    header dynamic content fully developed

--- themes/hotcoffee/header.php
+++ themes/hotcoffee/header.php
@@ -28,7 +28,7 @@
 		<div class="grid-x grid-padding-x grid-margin-x top-nav">
 			<div class="large-4 cell"></div>
 
-			<div class="large-4 cell site-branding">
+			<div class="large-4 small-12 cell site-branding">
 				<?php
 
 				if (! empty (get_custom_logo())){
@@ -56,10 +56,56 @@
 						?>
 			</div><!-- .site-branding -->
 
-			<div class="large-4 cell order-links">
-				<a href=""><img src="http://hotchat.local/wp-content/themes/hotcoffee/assets/img/search_icon.png"></a>&nbsp; &nbsp;
-				<a href=""><img src="http://hotchat.local/wp-content/themes/hotcoffee/assets/img/user.png"></a>&nbsp; &nbsp;
-				<a href=""><img src="http://hotchat.local/wp-content/themes/hotcoffee/assets/img/Cart.png"></a>
+			<!-- order links -->
+			<div class="order-links">
+				<ul class="large-4 small-12 cell order-menu">
+					<?php if (!empty (get_theme_mod('hotcoffee_login_url')) && !empty(get_theme_mod ('hotcoffee_login_title'))){ ?>
+						<li>
+							<a href="<?php echo esc_url (get_theme_mod('hotcoffee_login_url')); ?>">
+							<?php
+							if (!empty (get_theme_mod('hotcoffee_login_icon'))){
+								echo wp_get_attachment_image (get_theme_mod('hotcoffee_login_icon'), array('30', '30'));
+								echo get_theme_mod('hotcoffee_login_title');
+							} else {
+								echo get_theme_mod('hotcoffee_login_title');
+							}
+							?>
+							</a>
+						</li>
+
+					<?php } ?>
+
+					<?php if (!empty (get_theme_mod('hotcoffee_cart_url')) && !empty(get_theme_mod ('hotcoffee_cart_title'))){ ?>
+						<li>
+							<a href="<?php echo esc_url (get_theme_mod('hotcoffee_cart_url')); ?>">
+							<?php
+							if (!empty (get_theme_mod('hotcoffee_cart_icon'))){
+								echo wp_get_attachment_image (get_theme_mod('hotcoffee_cart_icon'), array('30', '30'));
+								echo get_theme_mod('hotcoffee_cart_title');
+							} else {
+								echo get_theme_mod('hotcoffee_cart_title');
+							}
+							?>
+							</a>
+						</li>
+
+					<?php } ?>
+
+				</ul>
+
+
+				<?php
+
+				/*
+					if (has_nav_menu( 'menu-shop' )){
+							wp_nav_menu(
+								array(
+									'theme_location' => 'menu-shop',
+								)
+							);
+					}
+				*/
+				?> 
 			</div>
 
 		</div><!-- close top nav -->
@@ -67,20 +113,53 @@
 		<nav id="site-navigation" class="main-navigation">
 			<div class="grid-x grid-padding-x grid-margin-x bottom-nav">
 				<div class="large-4 cell"></div>
-				<div class="large-4 cell menu-links">
+				<div class="large-4 small-12 cell menu-links">
 				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'hotcoffee' ); ?></button>
-				<?php
-				if (has_nav_menu( 'menu-primary' )){
-					wp_nav_menu(
-						array(
-							'theme_location' => 'menu-primary',
-							'menu_id'        => 'primary-menu',
-						)
-					);
-				}	
-				?>
-				</div>
-				<div class="large-4 cell"></div>
+					<?php
+						if (has_nav_menu( 'menu-primary' )){
+							wp_nav_menu(
+								array(
+									'theme_location' => 'menu-primary',
+									'menu_id'        => 'primary-menu',
+								)
+							);
+						}
+					?>
+				</div> <!-- Close primary menu -->
+
+				<!-- Search bar -->
+				<div class="large-4 cell">
+					<form>
+  						<input type="search" placeholder="Search...">
+  						<button type="submit">
+						  <?php if (!empty (get_theme_mod('hotcoffee_search_url')) && !empty(get_theme_mod ('hotcoffee_search_title'))){ ?>
+						
+							<a href="<?php echo esc_url (get_theme_mod('hotcoffee_search_url')); ?>">
+							<?php
+								if (!empty (get_theme_mod('hotcoffee_search_icon'))){
+									echo wp_get_attachment_image (get_theme_mod('hotcoffee_search_icon'), array('30', '30'));
+								} else {
+									echo get_theme_mod('hotcoffee_search_title');
+								}
+							?>
+							</a>
+
+						<?php } ?>
+						</button>
+					</form>
+
+					<?php
+					/*
+					if (has_nav_menu( 'menu-search' )){
+						wp_nav_menu(
+							array(
+								'theme_location' => 'menu-search',
+								'menu_id'        => 'search-menu',
+							)
+						);
+					} */
+					?>
+				</div><!-- close Search bar -->
         	</div>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->

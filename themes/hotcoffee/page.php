c80bc2f09ea48ed652314e158810438d4a704466
Vanathy <vanathy.gowreesan@dcmail.ca>
2022-03-29 15:11:38 -0400

    page.php made responsive

--- themes/hotcoffee/page.php
+++ themes/hotcoffee/page.php
@@ -16,21 +16,25 @@ get_header();
 ?>
 
 	<main id="primary" class="site-main">
+	<div class="grid-container">
+		<div class="grid-x grid-padding-x grid-margin-x">
+			<div class="large-12 medium-12 small-12 cell">
+			<?php
+				while ( have_posts() ) :
+					the_post();
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+					get_template_part( 'template-parts/content', 'page' );
 
-			get_template_part( 'template-parts/content', 'page' );
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
+				endwhile; // End of the loop.
+				?>
+			</div>
+		</div>
+	</div>
 	</main><!-- #main -->
 
 <?php

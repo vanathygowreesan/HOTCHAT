c6f651a9846e75271b696110cb95562f352db99a
Vanathy <vanathy.gowreesan@dcmail.ca>
2022-03-29 15:42:33 -0400

    index.php made responsive

--- themes/hotcoffee/index.php
+++ themes/hotcoffee/index.php
@@ -16,48 +16,52 @@ get_header();
 ?>
 
 	<main id="primary" class="site-main">
+	<div class="grid-container">
+		<div class="grid-x grid-padding-x grid-margin-x">
+			<div class="large-12 medium-12 small-12 cell">
+			<?php
+			// Conditional tags to check the posts
+			if ( have_posts() ) :
 
-	<div class="grid-x grid-padding-x grid-margin-x">
-		<div class="large-12 small-12 cell">
-		<?php
-		if ( have_posts() ) :
+				if ( is_home() && ! is_front_page() ) :
+					?>
+					<header>
+						<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+					</header>
+					<?php
+				endif;
 
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-				<?php
+				/* Start the Loop */
+				while ( have_posts() ) :
+					the_post();
+
+					/*
+				 	* Include the Post-Type-specific template for the content.
+				 	* If you want to override this in a child theme, then include a file
+				 	* called content-___.php (where ___ is the Post Type name) and that will be used instead.
+				 	*/
+					get_template_part( 'template-parts/content', get_post_type() );
+
+				endwhile;
+
+				the_posts_navigation();
+
+			else :
+
+				get_template_part( 'template-parts/content', 'none' );
+
 			endif;
+			?>
+			</div>
 
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
-
+			<!-- display side bar right-->
+			<div class="cell large-4 medium-4 small-12">
+				<?php get_sidebar(); ?>
+			</div>
 		</div>
 	</div>
-		
-	</main><!-- #main -->
+	</main><!-- close main -->
 
 <?php
-get_sidebar();
+//get_sidebar();
 get_footer();

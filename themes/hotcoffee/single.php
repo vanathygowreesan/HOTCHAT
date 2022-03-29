e651443cdcdb9389bd2a83a4b072cc848bb01901
Vanathy <vanathy.gowreesan@dcmail.ca>
2022-03-28 22:31:40 -0400

    single post page made responsive for all devices

--- themes/hotcoffee/single.php
+++ themes/hotcoffee/single.php
@@ -11,33 +11,41 @@ get_header();
 ?>
 
 	<main id="primary" class="site-main">
-	<div class="grid-x grid-padding-x grid-margin-x">
-		<div class="large-12 small-12 cell">
-		<?php
-		while ( have_posts() ) :
-			the_post();
 
-			get_template_part( 'template-parts/content', get_post_type() );
+	<div class="grid-container">
+		<div class="grid-x grid-padding-x grid-margin-x">
+			<div class="cell large-12 medium-12 small-12 ">
+			<?php
+			while ( have_posts() ) :
+				the_post();
 
-			the_post_navigation(
-				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'hotcoffee' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'hotcoffee' ) . '</span> <span class="nav-title">%title</span>',
-				)
-			);
+				get_template_part( 'template-parts/content', get_post_type() );
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+				the_post_navigation(
+					array(
+						'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'hotcoffee' ) . '</span> <span class="nav-title">%title</span>',
+						'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'hotcoffee' ) . '</span> <span class="nav-title">%title</span>',
+					)
+				);
+			?>
+			</div>
 
-		endwhile; // End of the loop.
-		?>
+			<div class="cell large-2 medium-2"></div>
 
-		<div>
+			<div class="cell large-8 medium-8 small-12 ">
+			<?php
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+
+			endwhile; // End of the loop.
+			?>
+			<div>
+		</div>
 	</div>
 	</main><!-- #main -->
 
 <?php
-get_sidebar();
+//get_sidebar();
 get_footer();

744e63e9055ee96cd64c1d50af12a0f00348b1b5
Vanathy <vanathy.gowreesan@dcmail.ca>
2022-03-29 09:36:29 -0400

    tested and commented custom archive post echo

--- themes/hotcoffee/archive-hotcoffee_recipe.php
+++ themes/hotcoffee/archive-hotcoffee_recipe.php
@@ -16,17 +16,18 @@ get_header();
 
 			<header class="page-header">
 				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
+				//the_archive_title( '<h1 class="page-title">', '</h1>' );
+				//the_archive_description( '<div class="archive-description">', '</div>' );
 				?>
 			</header><!-- .page-header -->
 
 			<?php
+
+			//echo'This is the archive recipe template';
+
 			/* Start the Loop */
 			while ( have_posts() ) :
 				the_post();
-
-				echo'This is the archive recipe template';
 
 				/*
 				 * Include the Post-Type-specific template for the content.
